aba99d7234eb523141966f42c1ed59b08428f1f1
SimoneS93 <salernosimone.uni@gmail.com>
2014-03-19 10:30:56 +0100

    Update categories.php

--- oo/categories.php
+++ oo/categories.php
@@ -2,25 +2,56 @@
 
 namespace data;
 
+/**
+ * Let's you work with categories 
+ */
+
 class categories extends base {
     private static $records;
     
+    /**
+     * Get the current category if is set
+     * @return model
+     */
     public static function current() {
         if ($slug = self::is_category())
             return array_pop(self::get('slug', $slug));
     }
+    
+    /**
+     * Get categories based on a callback filter
+     * @param type $filter
+     * @return type
+     */
+    public static function filter($filter) {
+        self::load();
+        return parent::_filter_callback(self::$records, $filter);
+    }
 
+    /**
+     * Retrieve all the categories or just one based on key-value pair
+     * @param string $key
+     * @param mixed $val
+     * @return array of models or a single model
+     */
     public static function get($key = '', $val = '') {
         self::load();
         return parent::_filter_keyval(self::$records, $key, $val);
     }
     
+    /**
+     * Check is the current page is a category page: if it is, returns its slug
+     * @return FALSE or category's slug
+     */
     public static function is_category() {
         if (strpos(current_url(), 'category/') === 0)
             return array_pop(explode('/', current_url()));
         return FALSE;
     }
 
+    /**
+     * Load once all the categories
+     */
     protected static function load() {
         if (!self::$records) {
             categories();
