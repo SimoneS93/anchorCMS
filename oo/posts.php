74c2a087079b27b925da62e3d3734ce41c2519c4
SimoneS93 <salernosimone.uni@gmail.com>
2014-03-19 10:38:43 +0100

    Update posts.php

--- oo/posts.php
+++ oo/posts.php
@@ -2,14 +2,37 @@
 
 namespace data;
 
+/**
+ * Let's you work with posts
+ */
+
 class posts extends base { 
     private static $records;
 
+    /**
+     * Get psts based on a callback filter
+     * @param type $filter
+     * @return type
+     */
+    public static function filter($filter) {
+        self::load();
+        return parent::_filter_callback(self::$records, $filter);
+    }
+
+    /**
+     * Retrieve all posts or just one based on key-value pair
+     * @param string $key
+     * @param mixed $val
+     * @return array of models or a single model
+     */
     public static function get($key = '', $val = '') {
         self::load();
         return parent::_filter_keyval(self::$records, $key, $val);
     }
 
+    /**
+     * Load once all the posts
+     */
     protected static function load() {
         if (!self::$records) {
             $posts = array_pop(\Post::listing(null, 1, 999));
@@ -25,10 +48,14 @@ class posts extends base {
         } 
     }
     
+    /**
+     * Load post's custom fields
+     * @param model $post
+     */
     private static function extend(&$post) {
         $post->content = parse($post->html);
         $page = \Registry::get('posts_page');
-	      $post->url = base_url($page->slug . '/' . $post->slug);
+	$post->url = base_url($page->slug . '/' . $post->slug);
         
         //add custom fields
         $extends = \Extend::fields('post');
