c16b6feac10f971955e44c7e6d79c8436200b2de
SimoneS93 <salernosimone.uni@gmail.com>
2014-03-30 17:16:12 +0200

    Update posts.php

--- oo/posts.php
+++ oo/posts.php
@@ -3,7 +3,7 @@
 namespace data;
 
 /**
- * Lets you work with posts
+ * Let's you work with posts
  */
 
 class posts extends base { 
@@ -35,14 +35,14 @@ class posts extends base {
      */
     protected static function load() {
         if (!self::$records) {
+	    #fetch from database
             $posts = array_pop(\Post::listing(null, 1, 999));
-            
+            #make model from each
             foreach ($posts as $post) {
                 $post = model::make($post);
-                
                 self::extend($post);
                 $post->delete('html');
-                
+                #push
                 self::$records[] = $post;
             } 
         } 
@@ -53,11 +53,15 @@ class posts extends base {
      * @param model $post
      */
     private static function extend(&$post) {
+        #parse markdwn
         $post->content = parse($post->html);
+        #fetch category
+        $category = array_shift(categories::get('id', $post->category));
+        $post->category_slug = $category->slug;
+        #compute url
         $page = \Registry::get('posts_page');
 	$post->url = base_url($page->slug . '/' . $post->slug);
-        
-        //add custom fields
+        #add custom fields
         $extends = \Extend::fields('post');
         foreach ($extends as $extend) {
             $key = $extend->key;
